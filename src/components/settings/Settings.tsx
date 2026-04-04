a075e4d928c72067132b7484d94ed9bb3e5cfa34
Gabor Lengyel <github@dromax.hu>
2026-04-04 22:30:27 +0200

    fix: remove 500 log limit option

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -174,7 +174,6 @@ export function Settings() {
             <option value={50}>50</option>
             <option value={100}>100</option>
             <option value={200}>200</option>
-            <option value={500}>500</option>
           </select>
         </div>
       </section>
